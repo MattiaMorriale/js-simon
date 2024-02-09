d5a4dea9df5d29f0e23e8369b3e28af2a68a5131
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-09 16:43:23 +0100

    fatto eventlistener

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const inviaElement = document.querySelector('#invia');
+
+const userdateElement = document.querySelector('#userdate')
+
+inviaElement.addEventListener('click', function(){
+    
+
+    
+})
