6ce2d1d82d543d30a8a7cf6d17ee19534299ec5b
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-09 16:56:27 +0100

    calcolo del tempo e stampa del documento in pagina

--- js/script.js
+++ js/script.js
@@ -16,6 +16,17 @@ inviaElement.addEventListener('click', function(){
 
     // calcolo della data scelta con quella odienra per vedere quanto manca
     var distance = countDownDate - now;
+
+    // calcolo del tempo con giorni, ore, minuti, secondi
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // stampa su schermo il countdown
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
     }, 1000);
     
 })
