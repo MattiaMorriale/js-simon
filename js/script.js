14179099380bc733d107130fa3f87cbf1eb3d01e
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-09 16:50:06 +0100

    creata intervallo

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@ const userdateElement = document.querySelector('#userdate')
 
 inviaElement.addEventListener('click', function(){
     
+    // selezionare la data 
+    var countDownDate = new Date("12 feb, 2024 9:30:00").getTime();
 
+    // diminusice il conutdown di 1 secondo
+    var x = setInterval(function() {
+
+    // data odierna
+    var now = new Date().getTime();
+
+    // calcolo della data scelta con quella odienra per vedere quanto manca
+    var distance = countDownDate - now;
+    }, 1000);
     
 })
